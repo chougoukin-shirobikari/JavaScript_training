77765378fa6f51b8ff6f298d4d1a3f8406ca46b0
mizuki <banana0822hairuyo@gmail.com>
2023-04-28 05:34:02 +0900

    2023/04/28 5:33

---
+++ 09/Sample3.js
@@ -0,0 +1,10 @@
+function down(){
+    var e = document.getElementById("description");
+    var t = document.createTextNode("写真のご説明をします。");
+    e.appendChild(t);
+}
+
+function up(){
+    var e = document.getElementById("description");
+    e.removeChild(e.lastChild);
+}
---
+++ 09/Sample4.js
@@ -0,0 +1,4 @@
+function change(picname){
+    var e = document.getElementById("bigimage");
+    e.setAttribute("src", picname);
+}
