935e090ee48064aa4e3355f558ac7c9aa2307715
mizuki <banana0822hairuyo@gmail.com>
2023-04-28 05:08:50 +0900

    2023/04/28 5:08

---
+++ 08/Sample2.js
@@ -0,0 +1,3 @@
+function welcome(){
+    window.open("blue.jpg", "サンプル", "width=100", "height=100");
+}
---
+++ 08/Sample6.js
@@ -0,0 +1,4 @@
+function change(){
+    var r = parseInt(Math.random() * 6);
+    document.pic.src = "pic" + r + ".jpg";
+}
