a03752c2ab337489779cb7dd6366b539fc510427
mizuki <banana0822hairuyo@gmail.com>
2023-04-30 19:58:33 +0900

    2023/04/30 19:58

---
+++ 12/Sample.js
@@ -0,0 +1,8 @@
+var a = navigator.userAgent;
+var ios = a.indexOf("iPhone") > 0 || a.indexOf("iPad") > 0 || a.indexOf("Android") > 0;
+var android = a.indexOf("Android") > 0;
+if(ios == true || android == true){
+    document.writeln('<link rel="stylesheet" href="SamplePhone.css">');
+}else{
+    document.writeln('<link rel="stylesheet" href="SamplePC.css">');
+}
